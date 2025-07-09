4ae61c5b57716da8212e74d708a7242612bc4187
Sarthak Thakur <115032052+tsarthak777@users.noreply.github.com>
2025-07-09 18:35:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
+// Initialize AOS (Animate On Scroll)
 AOS.init({
-  duration: 1000,
-  once: true
+  duration: 1000,     // Animation duration in ms
+  once: true,         // Run only once per element
+  easing: 'ease-in-out'
 });
